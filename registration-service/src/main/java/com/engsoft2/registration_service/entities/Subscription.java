1f2831e99159acdbbecad9b8d07a6bd5f0031fcb
Ana Xavier <anacarolina.oxavier@gmail.com>
2024-06-22 19:12:28 -0300

    adding the forgotten methods :(

--- registration-service/src/main/java/com/engsoft2/registration_service/entities/Subscription.java
+++ registration-service/src/main/java/com/engsoft2/registration_service/entities/Subscription.java
@@ -1,6 +1,5 @@
 package com.engsoft2.registration_service.entities;
 
-import com.engsoft2.registration_service.repositories.ApplicationRepository;
 import jakarta.persistence.*;
 
 import java.util.Date;
@@ -8,18 +7,48 @@ import java.util.Date;
 @Entity
 public class Subscription {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long subscription_id;
-	@ManyToOne
-	@JoinColumn(name = "application_id", nullable = false)
-	private Application application_id;
-	@ManyToOne
-	@JoinColumn(name = "client_id", nullable = false)
-	private Client client_id;
-	private Date begin_subscription;
-	private Date end_subscription;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long subscriptionId;
+    @ManyToOne
+    @JoinColumn(name = "application_id", nullable = false)
+    private Application application;
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+    private Date beginSubscription;
+    private Date endSubscription;
+    private String status;
 
+    public Long getSubscriptionId() {
+        return subscriptionId;
+    }
 
+    public Application getApplication() {
+        return application;
+    }
 
+    public Client getClient() {
+        return client;
+    }
+
+    public Date getBeginSubscription() {
+        return beginSubscription;
+    }
+
+    public Date getEndSubscription() {
+        return endSubscription;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setEndSubscription(Date endSubscription) {
+        this.endSubscription = endSubscription;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
