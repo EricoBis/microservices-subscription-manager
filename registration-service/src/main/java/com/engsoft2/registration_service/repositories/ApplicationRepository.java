720922b6eabdcb58a9de22fac2ef4945cc4e9fa7
Ana Xavier <anacarolina.oxavier@gmail.com>
2024-06-22 19:55:14 -0300

    IT'S DONE, I WON THIS GAME

--- registration-service/src/main/java/com/engsoft2/registration_service/repositories/ApplicationRepository.java
+++ registration-service/src/main/java/com/engsoft2/registration_service/repositories/ApplicationRepository.java
@@ -2,6 +2,7 @@ package com.engsoft2.registration_service.repositories;
 
 import com.engsoft2.registration_service.entities.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application,Long> {
