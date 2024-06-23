a3f794d71df43edaca8e1509ca47f46075153cf3
Érico Bis <ericobis.santos@gmail.com>
2024-06-23 19:43:59 -0300

    feat: added payment controller

---
+++ payment-service/src/main/java/com/engsof2/payment_service/adapters/controllers/PaymentController.java
@@ -0,0 +1,27 @@
+package com.engsof2.payment_service.adapters.controllers;
+
+import com.engsof2.payment_service.application.dto.PaymentDTO;
+import com.engsof2.payment_service.domain.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+
+    @PostMapping("/registrarpagamento")
+    public void registerPayment(@RequestBody PaymentDTO paymentDTO) {
+        paymentService.registerPayment(paymentDTO);
+    }
+
+}
